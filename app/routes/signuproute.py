dc449adfb1198759d07d9a08cb73cc2e2fcd4726
heyouyangs <kristine.hinoctan123@gmail.com>
2023-11-19 19:03:21 +0800

    sign up front end

--- app/routes/signuproute.py
+++ app/routes/signuproute.py
@@ -1,5 +1,6 @@
 from flask import *
 from flask_wtf import *
+from flask_dance.contrib.google import make_google_blueprint, google
 
 
 signup_bp = Blueprint('signup', __name__)
@@ -8,3 +9,4 @@ signup_bp = Blueprint('signup', __name__)
 @signup_bp.route('/')
 def home():
     return render_template('signup.html')
+
